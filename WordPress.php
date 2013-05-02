9e72d52102963fd9e876c1ea31c0330b823e4922
Christian Wach <needle@haystack.co.uk>
2013-05-02 14:53:05 +0100

    rewritten to enable Drupal-like hooks in WordPress

--- WordPress.php
+++ WordPress.php
@@ -34,78 +34,197 @@
  */
 class CRM_Utils_Hook_WordPress extends CRM_Utils_Hook {
 
-	function invoke(
-		$numParams,
-		&$arg1, &$arg2, &$arg3, &$arg4, &$arg5,
-		$fnSuffix
-	) {
+  /**
+   * @var bool
+   */
+  private $isBuilt = FALSE;
 
-		// legacy support test for existing commonInvoke hooks, which are always
-		// prefixed by 'wordpress_'
-		$fnName = "wordpress_{$fnSuffix}";
-		if (function_exists($fnName)) {
-			return $this->commonInvoke(
-				$numParams,
-				$arg1, $arg2, $arg3, $arg4, $arg5,
-				$fnSuffix, 'wordpress'
-			);
-		}		
+  /**
+   * @var array(string)
+   */
+  private $allModules = NULL;
 
-		// only pass the arguments that have values
-		$args = array_slice( 
-			array( &$arg1, &$arg2, &$arg3, &$arg4, &$arg5 ), 
-			0, 
-			$numParams
-		);
-		
-		// use WordPress API to run hooks
-		// uncomment in the real world and delete everything below
-		//return apply_filters_ref_array( $fnSuffix, $args );
+  /**
+   * @var array(string)
+   */
+  private $civiModules = NULL;
 
-		// to test return values
-		$return = apply_filters_ref_array( $fnSuffix, $args );
+  /**
+   * @var array(string)
+   */
+  private $wordpressModules = NULL;
 
-		/*
-		// testing 'hook_civicrm_pre', for example
-		if ( $fnSuffix == 'civicrm_pre' ) {
+  /**
+   * @var array(string)
+   */
+  private $excludedHooks = array(
+    'civicrm_upgrade',
+    'civicrm_validate',
+    'civicrm_validateForm',
+    'civicrm_caseSummary',
+    'civicrm_dashboard',
+    'civicrm_links',
+    'civicrm_aclWhereClause',
+    'civicrm_alterSettingsMetaData'
+  );
 
-			// trace stuff
-			print_r( "\n\n" );
-			print_r( 'invoke-post-apply_filters_ref_array' ); 
+  /**
+   * CMW: because hooks are called by referencing the class (and the methods are not
+   * abstract) no overloading can be done in CRM_Utils_Hook_WordPress. As a result,
+   * only the invoke() method is ever available as a way to intercept and adapt
+   * hook implementations. That's (sort of) okay, because the hook name is always
+   * passed, so we can route different kind of hooks to different private methods
+   * for processing if needed
+   */
 
-			print_r( "\n\n" );
-			print_r( 'returned value'."\n" ); 
-			print_r( $return ); 
+  public function invoke(
+    $numParams,
+    &$arg1, &$arg2, &$arg3, &$arg4, &$arg5,
+    $fnSuffix
+  ) {
+    
+    // only pass the arguments that have values
+    $args = array_slice( 
+      array( &$arg1, &$arg2, &$arg3, &$arg4, &$arg5 ), 
+      0, 
+      $numParams
+    );
+    
+    
+    
+    /**
+     * CMW: do_action_ref_array is the default way of calling WordPress hooks 
+     * because for the most part no return value is wanted. However, this is 
+     * only generally true, so using do_action_ref_array() is only called for those 
+     * hooks which do not require a return value. We exclude the following, which
+     * are incompatible with the WordPress Plugin API:
+     * 
+     * civicrm_upgrade
+     * http://wiki.civicrm.org/confluence/display/CRMDOC43/hook_civicrm_upgrade
+     * 
+     * civicrm_validate
+     * http://wiki.civicrm.org/confluence/pages/viewpage.action?pageId=79888748
+     * civicrm_validate is deprecated but still in use:
+     * eg, line 301 of civicrm/CRM/Core/Form.php
+     * 
+     * civicrm_validateForm
+     * http://wiki.civicrm.org/confluence/display/CRMDOC43/hook_civicrm_validateForm
+     * 
+     * civicrm_caseSummary
+     * http://wiki.civicrm.org/confluence/display/CRMDOC43/hook_civicrm_caseSummary
+     * 
+     * civicrm_dashboard
+     * http://wiki.civicrm.org/confluence/display/CRMDOC43/hook_civicrm_dashboard
+     * 
+     * civicrm_links
+     * http://wiki.civicrm.org/confluence/display/CRMDOC43/hook_civicrm_links
+     * 
+     * civicrm_aclWhereClause
+     * http://wiki.civicrm.org/confluence/display/CRMDOC43/hook_civicrm_aclWhereClause
+     * 
+     * civicrm_alterSettingsMetaData
+     * civicrm_alterSettingsMetaData expects a return value, although there are no docs
+     * eg, line 595 of civicrm/CRM/Core/BAO/Setting.php
+     * http://wiki.civicrm.org/confluence/pages/viewpage.action?pageId=80806000
+     */
+    
+    // distinguish between types of hook
+    if ( ! in_array( $fnSuffix, $this->excludedHooks ) ) {
+      
+        // ---------------------------------------------------------------------
+        // use WordPress Plugins API to modify $args
+        // ---------------------------------------------------------------------
+        // Because $args are passed as references to the WordPress callbacks, 
+        // runHooks subsequently receives appropriately modified parameters.
+        do_action_ref_array( $fnSuffix, $args );
+        
+    }
+    
+    
+    
+    /*
+     * CMW: I'm following the logic of the Joomla hook file by allowing WordPress 
+     * callbacks to do their stuff before runHooks gets called. I'm following the
+     * logic of the Drupal hook file by building the "module" (read "plugin") list
+     * and then calling runHooks directly. This should side-step the need for the
+     * post-processing that the Joomla hook file does.
+     * 
+     * Note that hooks which require a return value are incompatible with the 
+     * signature of apply_filters_ref_array and must therefore be called in 
+     * global scope, like in Drupal. It's not ideal, but plugins can always route 
+     * these calls to methods in their classes.
+     * 
+     * At some point, those hooks could be pre-processed and called via the WordPress
+     * Plugin API, but it would change their signature and require the CiviCRM docs
+     * to be rewritten for those calls in WordPress. So I've done it this way for
+     * now.
+     */
+    
+    // build list of registered plugin codes
+    $this->buildModuleList();
+    
+    // -------------------------------------------------------------------------
+    // Call runHooks the same way Drupal does
+    // -------------------------------------------------------------------------
+    $moduleResult = $this->runHooks(
+      $this->allModules, 
+      $fnSuffix,
+      $numParams, 
+      $arg1, $arg2, $arg3, $arg4, $arg5
+    );
 
-			print_r( "\n\n" );
-			print_r( 'arg1'."\n" ); 
-			print_r( $arg1 ); 
+    // finally, return
+    return empty($moduleResult) ? TRUE : $moduleResult;
+    
+  }
+  
 
-			print_r( "\n\n" );
-			print_r( 'arg2'."\n" ); 
-			print_r( $arg2 ); 
 
-			print_r( "\n\n" );
-			print_r( 'arg3'."\n" ); 
-			print_r( $arg3 ); 
+  /**
+   * Build the list of plugins ("modules" in CiviCRM terminology) to be processed for hooks.
+   * We need to do this to preserve the CiviCRM hook signatures for hooks that require
+   * a return value, since AFAICT the WordPress Plugin API is incompatible with them.
+   * 
+   * Copied and adapted from: CRM/Utils/Hook/Drupal6.php
+   */
+  function buildModuleList() {
+    if ($this->isBuilt === FALSE) {
+    
+      if ($this->wordpressModules === NULL) {
+        
+        // include custom PHP file - copied from parent->commonBuildModuleList()
+        $config = CRM_Core_Config::singleton();
+        if (!empty($config->customPHPPathDir) &&
+          file_exists("{$config->customPHPPathDir}/civicrmHooks.php")
+        ) {
+          @include_once ('civicrmHooks.php');
+        }
 
-			print_r( "\n\n" );
-			print_r( 'arg4'."\n" ); 
-			print_r( $arg4 ); 
+        // initialise with the pre-existing 'wordpress' prefix
+        $this->wordpressModules = array('wordpress');
+        
+        // use WordPress Plugin API to build list
+        // a plugin simply needs to declare its "unique_plugin_code" thus:
+        // add_filter('civicrm_wp_plugin_codes', 'function_that_returns_my_unique_plugin_code');
+        $this->wordpressModules = apply_filters('civicrm_wp_plugin_codes', $this->wordpressModules);
+      }
 
-			print_r( "\n\n" );
-			print_r( 'arg5'."\n" ); 
-			print_r( $arg5 ); 
+      if ($this->civiModules === NULL) {
+        $this->civiModules = array();
+        $this->requireCiviModules($this->civiModules);
+      }
 
-			die();
+      $this->allModules = array_merge((array)$this->wordpressModules, (array)$this->civiModules);
+      if ($this->wordpressModules !== NULL && $this->civiModules !== NULL) {
+        // both CRM and CMS have bootstrapped, so this is the final list
+        $this->isBuilt = TRUE;
+      }
+      
+    }
+  }
 
-		}
-		*/
 
-		// --<
-		return $return;
-
-	}
 
 }
+
 
--- civicrm-wp-hook-tester.php
+++ civicrm-wp-hook-tester.php
@@ -1,112 +1,90 @@
 <?php
 /*
 Plugin Name: CiviCRM Hook Tester
-Description: Just for testing...
+Description: For testing, um, CiviCRM hooks...
 Author: Christian Wach
-Version: 0.1
+Version: 0.2
 Author URI: http://haystack.co.uk
 */
 
 
 
 /**
- * Multiple add_filter test, mimicking hook_civicrm_managed()
- */
-function my_test_callback( $entities ) {
-	
-	$entities[] = array(
-		'module' => 'com.example.modulename',
-		'name' => 'myreport',
-		'entity' => 'ReportTemplate',
-		'params' => array(
-			'version' => 3,
-			'label' => 'Example Report',
-			'description' => 'Longish description of the example report',
-			'class_name' => 'CRM_Modulename_Report_Form_Sybunt',
-			'report_url' => 'mymodule/mysbunt',
-			'component' => 'CiviContribute',
-		),
-	);
-	
-	// --<
-	return $entities;
+--------------------------------------------------------------------------------
+Notes for developers
+--------------------------------------------------------------------------------
 
-}
 
-// uncomment to add actions for the above
-//add_filter( 'blah', 'my_test_callback', 10, 1 );
-//add_filter( 'blah', 'my_test_callback', 11, 1 );
-//add_filter( 'blah', 'my_test_callback', 12, 1 );
 
-/*
-// start with an empty array
-$arg1 = array();
+----------------------------
+CALLBACKS WITH RETURN VALUES
+----------------------------
 
-// mimic how CRM_Utils_Hook_WordPress::invoke parses parameters
-$x = array_slice( array( &$arg1 ), 0, 1 );
+Because of the way CiviCRM hooks are constructed, the WordPress Plugin API cannot 
+be used for any hooks that require a return value, because they are seemingly 
+incompatible with apply_filters and apply_filters_ref_array.
 
-// trace
-print_r( "\n\n" );
-print_r( "pre-hook\n" );
-print_r( $arg1 );
+Luckily, there are very few of them at present. The full list is:
 
-// run hook
-$return = apply_filters_ref_array( 'blah', $x );
+'civicrm_upgrade'
+'civicrm_validate' (obsolete in CiviCRM 4.3)
+'civicrm_validateForm'
+'civicrm_caseSummary'
+'civicrm_dashboard'
+'civicrm_links'
+'civicrm_aclWhereClause'
+'civicrm_alterSettingsMetaData'
 
-// trace
-print_r( "\n\n" );
-print_r( "post-hook\n" );
+Any plugins that need to have callbacks for these hooks must register a unique code 
+with CiviCRM. The code must be a valid part of a PHP function name, so stick to
+letters and underscores. This allows them to use global scope functions as callbacks 
+in the form:
 
-print_r( "\n\n" );
-print_r( "arg1\n" );
-print_r( $arg1 );
+{code}_{hookname}
 
-print_r( "\n\n" );
-print_r( "return\n" );
-print_r( $return );
-die();
+This plugin uses the prefix 'my_unique_plugin_code' and the callback function name 
+for 'hook_civicrm_links' thus becomes:
+
+'my_unique_plugin_code_civicrm_links'
+
+If a plugin needs to keep its logic inside a class, it will have to route the call 
+to the class method from the global function. 
+
+If you want to set a priority for the callback, well, good luck.
+
+Yes, Drupal-style hooks in WordPress, oh joy.
+
+
+
+-------------------------------
+CALLBACKS WITH NO RETURN VALUES
+-------------------------------
+
+However, if your plugin doesn't need to create callbacks for any of these hooks, 
+you can simply use the WordPress Plugin API as per usual:
+
+add_action( 'civicrm_hookname', array( $this, 'my_callback' ), $priority, $num_args );
+
+So, to hook into what CiviCRM's docs call 'hook_civicrm_tabs' (and you wanted to do
+so with a priority) you would use something like:
+
+add_action( 'civicrm_tabs', array( $this, 'my_callback' ), 20, 2 );
+
+Yay!
+
+Just remember that because the parameters are passed by reference, you can modify
+them in your callback if you need to and that modification will be passed on to the
+next callback in its modified state.
+
+--------------------------------------------------------------------------------
 */
 
 
 
-
-
 /**
- * Global scope callback function
- * 
- * Note that CiviCRM expects $params to be passed by reference, but that
- * do_action_ref_array passes by value
+ * CiviCRM_WordPress_Hook_Tester Class
  */
-function my_test_civi_callback( $op, $objectName, $id, $params ) {
-	
-	/*
-	// trace
-	print_r( "\n\n" );
-	print_r( 'my_test_civi_callback'."\n" ); 
-	print_r( array( $op, $objectName, $id, $params ) ); 
-	die();
-	*/
-	
-	// add something
-	$params['foo'] = 'bar'; 
-	
-	// --<
-	return $params;
-	
-}
-
-// uncomment to add action for the above
-//add_filter( 'civicrm_pre', 'my_test_civi_callback', 10, 4 );
-
-
-
-
-
-
-/**
- * TestingCiviCrmWordPressHooks Class
- */
-class TestingCiviCrmWordPressHooks {
+class CiviCRM_WordPress_Hook_Tester {
 
 
 
@@ -115,10 +93,23 @@ class TestingCiviCrmWordPressHooks {
 	 * @return object
 	 */
 	function __construct() {
-	
+		
+		// register this plugin's unique code with CiviCRM so it can receive callbacks
+		// for certain hooks. Given that CiviCRM requires PHP5.3, you could return this
+		// with an anonymous function, but I've resisted for clarity.
+		add_filter( 'civicrm_wp_plugin_codes', array( $this, 'register_my_civicrm_plugin_code' ) );
+
 		// add class method to hook
-		add_action( 'civicrm_pre', array( $this, 'my_test_civi_callback' ), 10, 4 );
-		add_action( 'civicrm_pre', array( $this, 'my_test_civi_callback2' ), 11, 4 );
+		add_action( 'civicrm_config', array( $this, 'my_civi_config_callback' ), 10, 1 );
+		add_action( 'civicrm_config', array( $this, 'my_civi_config_callback2' ), 11, 1 );
+		
+		// add class method to hook
+		add_action( 'civicrm_pre', array( $this, 'my_civi_pre_callback' ), 10, 4 );
+		add_action( 'civicrm_pre', array( $this, 'my_civi_pre_callback2' ), 11, 4 );
+		
+		// add class method to hook
+		add_action( 'civicrm_tabs', array( $this, 'my_civi_tabs_callback' ), 10, 2 );
+		//add_action( 'civicrm_pre', array( $this, 'my_civi_pre_callback2' ), 11, 4 );
 		
 		// --<
 		return $this;
@@ -128,27 +119,14 @@ class TestingCiviCrmWordPressHooks {
 
 
 	/** 
-	 * @description: class method as callback
-	 * @return object
+	 * @description: class method as callback for 'civicrm_wp_plugin_codes'
+	 * @return array
 	 */
-	function my_test_civi_callback( $op, $objectName, $id, $params ) {
+	function register_my_civicrm_plugin_code( $plugin_codes ) {
 	
-		/*
-		// trace
-		print_r( "\n\n" );
-		print_r( 'TestingCiviCrmWordPressHooks my_test_civi_callback'."\n" ); 
-		print_r( array( $op, $objectName, $id, $params ) ); 
-		die();
-		*/
-		
-		// let's see what happens
-		//$op .= '-opone';
-		//$objectName .= '-one';
-		//$id++;
-		$params['foo'] = 'bar'; 
-		
-		// --<
-		return $params;
+		// add our unique code and send back to CiviCRM
+		$plugin_codes[] = 'my_unique_civicrm_plugin_code';
+		return $plugin_codes;
 		
 	}
 
@@ -158,24 +136,115 @@ class TestingCiviCrmWordPressHooks {
 	 * @description: class method as callback
 	 * @return object
 	 */
-	function my_test_civi_callback2( $op, $objectName, $id, $params ) {
+	function my_civi_config_callback( $config ) {
 	
+		// let's see what happens
+		$config->foo = 'foo'; 
+		
 		/*
 		// trace
 		print_r( "\n\n" );
-		print_r( 'TestingCiviCrmWordPressHooks my_test_civi_callback2'."\n" ); 
-		print_r( array( $op, $objectName, $id, $params ) ); 
+		print_r( 'CiviCRM_WordPress_Hook_Tester my_civi_config_callback'."\n" ); 
+		print_r( array( 'config' => $config ) ); 
 		die();
 		*/
 		
+	}
+
+
+
+	/** 
+	 * @description: class method as callback
+	 * @return object
+	 */
+	function my_civi_config_callback2( $config ) {
+	
 		// let's see what happens
-		//$op .= '-optwo';
-		//$objectName .= '-two';
-		//$id++;
-		$params['foo2'] = 'bar2'; 
+		$config->bar = 'bar'; 
 		
-		// --<
-		return $params;
+		/*
+		// trace
+		print_r( "\n\n" );
+		print_r( 'CiviCRM_WordPress_Hook_Tester my_civi_config_callback2'."\n" ); 
+		print_r( array( 'config' => $config ) ); 
+		die();
+		*/
+		
+	}
+
+
+
+	/** 
+	 * @description: class method as callback
+	 * @return object
+	 */
+	function my_civi_pre_callback( $op, $objectName, $id, &$params ) {
+	
+		// let's see what happens
+		$params['foo'] = 'foo'; 
+		
+		/*
+		// trace
+		print_r( "\n\n" );
+		print_r( 'CiviCRM_WordPress_Hook_Tester my_civi_pre_callback'."\n" ); 
+		print_r( array( 'op' => $op, 'objectName' => $objectName, 'id' => $id, 'params' => $params ) ); 
+		die();
+		*/
+		
+	}
+
+
+
+	/** 
+	 * @description: class method as callback
+	 * @return object
+	 */
+	function my_civi_pre_callback2( $op, $objectName, $id, &$params ) {
+	
+		// let's see what happens
+		$params['bar'] = 'bar'; 
+		
+		/*
+		// trace
+		print_r( "\n\n" );
+		print_r( 'CiviCRM_WordPress_Hook_Tester my_civi_pre_callback2'."\n" ); 
+		print_r( array( 'op' => $op, 'objectName' => $objectName, 'id' => $id, 'params' => $params ) ); 
+		die();
+		*/
+		
+	}
+
+
+
+	/** 
+	 * @description: class method as callback for 'hook_civicrm_tabs'
+	 * @return object
+	 */
+	function my_civi_tabs_callback( &$tabs, $contactID ) {
+	
+		// example taken from the CiviCRM docs for this hook:
+		// http://wiki.civicrm.org/confluence/display/CRMDOC43/hook_civicrm_tabs
+ 
+		// unset the contribition tab, i.e. remove it from the page
+		unset( $tabs[1] );
+ 
+		// let's add a new "contribution" tab with a different name and put it last
+		// this is just a demo, in the real world, you would create a url which would
+		// return an html snippet etc.
+		$url = CRM_Utils_System::url( 'civicrm/contact/view/contribution',
+									  "reset=1&snippet=1&force=1&cid=$contactID" );
+		$tabs[] = array( 'id'    => 'mySupercoolTab',
+						 'url'   => $url,
+						 'title' => 'Contribution Tab Renamed',
+						 'weight' => 300 );
+
+		/*
+		// trace
+		print_r( "\n\n" );
+		print_r( 'CiviCRM_WordPress_Hook_Tester my_civi_tabs_callback'."\n" ); 
+		print_r( array( 'tabs' => $tabs, 'contactID' => $contactID ) ); 
+		die();
+		*/
 		
 	}
 
@@ -183,5 +252,77 @@ class TestingCiviCrmWordPressHooks {
 
 } // class ends
 
-// uncomment to instantiate plugin class above
-//$testing_civicrm_wordpress_hooks = new TestingCiviCrmWordPressHooks;
+
+
+// instantiate plugin class above
+$testing_civicrm_wordpress_hooks = new CiviCRM_WordPress_Hook_Tester;
+
+
+
+/**
+--------------------------------------------------------------------------------
+Global scope callbacks
+--------------------------------------------------------------------------------
+*/
+
+/** 
+ * @description: global function as callback for 'hook_civicrm_links'
+ * @return object
+ */
+function my_unique_civicrm_plugin_code_civicrm_links( $op, $objectName, $objectId, &$links ) {
+
+	// example taken from the CiviCRM docs for this hook:
+	// http://wiki.civicrm.org/confluence/display/CRMDOC43/hook_civicrm_links
+
+	/*
+	// trace
+	print_r( "\n\n" );
+	print_r( 'my_unique_civicrm_plugin_code_civicrm_links'."\n" ); 
+	print_r( array( 'op' => $op, 'objectName' => $objectName, 'objectId' => $objectId, 'links' => $links ) ); 
+	//die();
+	*/
+	
+	// init return
+	$mylinks = array();
+	
+	switch ($objectName) {
+
+		case 'Contact':
+
+			switch ($op) {
+				
+				// place link in the menu that is accessible from the "Actions" button 
+				// at top left on a Contact's page
+				case 'view.contact.activity':
+					// Adds a link to the main tab.
+					$my_links[] = array(
+						'id' => 'mymoduleactions',
+						'title' => 'My Module Actions',
+						'url' => '/'. $objectId,
+						'weight' => 1,
+						'ref'    => 'my-module-link' // this is unique identifier for this link
+					);
+					break;
+				
+				// I think this is a Drupal block
+				case 'create.new.shortcuts':
+					// add link to create new profile
+					$links[] = array( 
+						'url'   => '/civicrm/admin/uf/group?action=add&reset=1',
+						'title' => ts('New Profile'),
+						'ref'   => 'new-profile'
+					);
+					break;
+					
+			}
+
+	}
+	
+	
+	// return our links
+	return $my_links;
+	
+}
+
+
+
